a34f042280d29993704f9281861cf9623b05395c
phubner <phubner@grupopanvel.com.br>
2023-09-10 12:18:12 -0300

    refactor: deleting post pet route.

--- routes/pet-route.js
+++ routes/pet-route.js
@@ -1,14 +1,6 @@
 const petService = require("../services/pet-service");
 
 module.exports = function (app) {
-    app.post('/pet', (req, res) => {
-        const { name, breed, age } = req.body;
-        if (!name || !breed || !age) {
-            res.status(400).send({ message: 'Todos os campos são obrigatórios' });
-        }
-        res.status(201).json({ name, breed, age });
-    });
-
     app.get('/pet', (req, res) => {
         let obj = petService.getPets();
         res.status(200).send(obj);
